db453c497c0052fdf3b379c5b734b6c1b0447de4
thelonehegelian <thelonehegelian@gmail.com>
2023-08-13 04:05:03 +0100

    Roll back changes

---
+++ app/packages/nextjs/generated/deployedContracts.ts
@@ -0,0 +1,43 @@
+const contracts = {
+  31337: [
+    {
+      chainId: "31337",
+      name: "localhost",
+      contracts: {
+        SimpleStorage: {
+          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+          abi: [
+            {
+              inputs: [],
+              name: "retrieve",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "num",
+                  type: "uint256",
+                },
+              ],
+              name: "store",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
+} as const;
+
+export default contracts;

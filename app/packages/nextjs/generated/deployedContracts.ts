5dd5d5544a4a847278c1c0c032bfc086165e91e4
thelonehegelian <thelonehegelian@gmail.com>
2023-08-15 01:20:14 +0100

    add productfactory contract on optimism to generated

--- app/packages/nextjs/generated/deployedContracts.ts
+++ app/packages/nextjs/generated/deployedContracts.ts
@@ -38,6 +38,74 @@ const contracts = {
       },
     },
   ],
+  420: [
+    {
+      chainId: "420",
+      name: "optimismGoerli",
+      contracts: {
+        ProductFactory: {
+          address: "0x8D50094410571c85bcAA71D7fA52FaCA68043DD3",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "string",
+                  name: "_productName",
+                  type: "string",
+                },
+                {
+                  internalType: "string",
+                  name: "_productDescription",
+                  type: "string",
+                },
+                {
+                  internalType: "string",
+                  name: "mainImage",
+                  type: "string",
+                },
+              ],
+              name: "createProduct",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "deployedProducts",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "getDeployedProducts",
+              outputs: [
+                {
+                  internalType: "address[]",
+                  name: "",
+                  type: "address[]",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
 } as const;
 
 export default contracts;
